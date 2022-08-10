0ef6444793dd9fe5dc9c9e18076b078f89515839
dhk767 <dhk767@gmail.com>
2022-08-10 18:14:29 +0900

    marker center fix, add address search button

---
+++ src/main/java/com/ideaBANK/dongHyeon/DongHyeonApplication.java
@@ -0,0 +1,13 @@
+package com.ideaBANK.dongHyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongHyeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongHyeonApplication.class, args);
+	}
+
+}
